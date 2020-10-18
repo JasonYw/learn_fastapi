71a0ef1ed677425172ce36d1240c9f155c974119
rico <jasonwu@outlook.com>
2020-10-18 17:11:36 +0800

    1

--- ep0/ep1.py
+++ ep0/ep1.py
@@ -14,13 +14,30 @@ class Item_model(BaseModel):
     '''
         创建数据模型
         其中有默认值的为可选参数，description与tax
+        optional不要轻易加
     '''
     name:str
-    description:Optional[str] =None
+    description:str = None
     price:float
-    tax: Optional[float] =None
+    tax: float = None
 
 @app.post("/items/")
 async def create_item(item:Item_model):
-    return item
+    '''
+    return的是item不是Item_model
+    结果
+        将请求体作为json读取
+        转换为相应的数据类型
+        校验数据
+            数据无效会返回错误信息
+        接收的数据会放在item中
+        可将接收的item对象转换成字典类型
+    '''
+    item_dict =item.dict()
+    if item.tax:
+        price_with_tax =item.price+item.tax
+        item_dict.update({
+            "price_with_tax":price_with_tax
+        })
+    return item_dict
     
